c3eab0695221d18ac8b271e0bd3d63207586f409
Chichi Chichi <djimgoupat@yahoo.fr>
2016-12-27 22:01:25 +0100

    Update von private Field  and getters  to avoid the  Firebase Exception : DatabaseException: Found two getters or fields with conflicting case sensitivity

--- app/src/main/java/org/KeepActive/MIF_Aalen/helper/ActivityObject_Json.java
+++ app/src/main/java/org/KeepActive/MIF_Aalen/helper/ActivityObject_Json.java
@@ -11,15 +11,15 @@ import com.google.firebase.database.IgnoreExtraProperties;
 public class ActivityObject_Json {
 
 
-    public String Activity_Name;
-    public String Activity_Type;
-    public String Activity_Description;
-    public String Activity_Created_by;
-    public String activity_Created_at;
-    public String Street ;
-    public int cityCode;
-    public double LatCoord ;
-    public double LonCoord ;
+    private String Act_Name;
+    private String Act_Type;
+    private String Act_Description;
+    private String Act_Created_by;
+    private String Created_at;
+    private String Act_Street ;
+    private int cityCode;
+    private double Act_LatCoord ;
+    private double Act_LonCoord ;
 
     // Default constructor required for calls to
     // DataSnapshot.getValue(ActivityObject_Json.class)
@@ -28,72 +28,72 @@ public class ActivityObject_Json {
     public ActivityObject_Json(String activity_Name,
                                String activity_Type,
                                String activity_Description,
-                               String activity_Created_by,
-                               String activity_Created_at,
-                               String street,
-                               int cityCode,
-                               double latCoord,
-                               double lonCoord) {
+                               String Created_by,
+                               String Created_at,
+                               String param_street,
+                               int param_cityCode,
+                               double param_latCoord,
+                               double param_lonCoord) {
 
 
-        this.Activity_Name = activity_Name;
-        this.Activity_Type = activity_Type;
-        this.Activity_Description = activity_Description;
-        this.Activity_Created_by = activity_Created_by;
-        this.activity_Created_at = activity_Created_at;
-        this.Street = street;
+        this.Act_Name = activity_Name;
+        this.Act_Type = activity_Type;
+        this.Act_Description = activity_Description;
+        this.Act_Created_by = Created_by;
+        this.Created_at = Created_at;
+        this.Act_Street = param_street;
         this.cityCode = cityCode;
-        this.LatCoord = latCoord;
-        this.LonCoord = lonCoord;
+        this.Act_LatCoord = param_latCoord;
+        this.Act_LonCoord = param_lonCoord;
     }
 
 
     public String getActivity_Name() {
-        return Activity_Name;
+        return this.Act_Name;
     }
 
     public void setActivity_Name(String activity_Name) {
-        Activity_Name = activity_Name;
+        this.Act_Name = activity_Name;
     }
 
     public String getActivity_Type() {
-        return Activity_Type;
+        return this.Act_Type;
     }
 
     public void setActivity_Type(String activity_Type) {
-        Activity_Type = activity_Type;
+        this.Act_Type = activity_Type;
     }
 
     public String getActivity_Description() {
-        return Activity_Description;
+        return this.Act_Description;
     }
 
-    public void setActivity_Description(String activity_Description) {
-        Activity_Description = activity_Description;
+    public void setActivity_Description(String Description) {
+        this.Act_Description = Description;
     }
 
     public String getActivity_Created_by() {
-        return Activity_Created_by;
+        return this.Act_Created_by;
     }
 
-    public void setActivity_Created_by(String activity_Created_by) {
-        Activity_Created_by = activity_Created_by;
+    public void setActivity_Created_by(String Created_by) {
+        this.Act_Created_by = Created_by;
     }
 
     public String getActivity_Created_at() {
-        return activity_Created_at;
+        return this.Created_at;
     }
 
-    public void setActivity_Created_at(String activity_Created_at) {
-        this.activity_Created_at = activity_Created_at;
+    public void setActivity_Created_at(String Created_at) {
+        this.Created_at = Created_at;
     }
 
     public String getStreet() {
-        return Street;
+        return this.Act_Street;
     }
 
-    public void setStreet(String street) {
-        Street = street;
+    public void setStreet(String param_street) {
+        this.Act_Street = param_street;
     }
 
     public int getCityCode() {
@@ -105,18 +105,18 @@ public class ActivityObject_Json {
     }
 
     public double getLatCoord() {
-        return LatCoord;
+        return this.Act_LatCoord;
     }
 
-    public void setLatCoord(float latCoord) {
-        LatCoord = latCoord;
+    public void setLatCoord(double param_latCoord) {
+        this.Act_LatCoord = param_latCoord;
     }
 
     public double getLonCoord() {
-        return LonCoord;
+        return this.Act_LonCoord;
     }
 
-    public void setLonCoord(float lonCoord) {
-        LonCoord = lonCoord;
+    public void setLonCoord(double param_lonCoord) {
+        this.Act_LonCoord = param_lonCoord;
     }
 }
